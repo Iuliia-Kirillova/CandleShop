ea53a7efd1353ec91bb5883817fec9fa779dec04
Iuliia Kirillova <yvkirillova@yandex.ru>
2022-02-01 19:58:23 +0300

    Nice header + footer

--- views/common/footer.php
+++ views/common/footer.php
@@ -5,6 +5,35 @@
     <button class="button cookie_accept btn btn-primary btn-sm">Я согласен</button>
 </div>
 
+<div class="container">
+    <footer class="py-5">
+
+        <div class="d-flex justify-content-between py-4 my-4 border-top">
+            <p>&copy; 2022 Iuliia Kirillova</p>
+            <ul class="list-unstyled d-flex">
+                <li class="ms-3"><a class="link-dark" href="#"><img src="<?= LIBS . 'fontawesome/svgs/brands/vk.svg'; ?>"
+                                                                    width="24" height="24">
+                        <use xlink:href="#twitter"/>
+                        </svg></a></li>
+                <li class="ms-3"><a class="link-dark" href="#">
+                        <img src="<?= LIBS . 'fontawesome/svgs/brands/instagram.svg'; ?>" width="24" height="24">
+                            <use xlink:href="#instagram"/>
+                        </svg>
+                    </a></li>
+                <li class="ms-3"><a class="link-dark" href="#">
+                        <img src="<?= LIBS . 'fontawesome/svgs/brands/whatsapp.svg'; ?>" class="bi" width="24" height="24">
+                            <use xlink:href="#facebook"/>
+                        </img>
+                    </a></li>
+            </ul>
+        </div>
+    </footer>
+</div>
+
 <script src="<?= JS . 'script.js'; ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
 </body>
 </html>
